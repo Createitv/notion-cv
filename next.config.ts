e46be0844b010387e51ff3da947e47b70329d793
PangHuang <tnt@panghuang.local>
2024-11-13 23:14:37 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
+	// Configure `pageExtensions` to include markdown and MDX files
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		// ignoreBuildErrors: true,
+	},
+	reactStrictMode: true,
+	output: 'standalone',
+	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+}
 
-export default nextConfig;
+export default nextConfig
